b48b11efec0b4aab5c848cabe4eda8e1c39e10c1
54m <30588003+54m@users.noreply.github.com>
2021-03-19 20:30:53 +0900

    fix: godoc

--- pkg/constant/constant.go
+++ pkg/constant/constant.go
@@ -2,7 +2,7 @@ package constant
 
 const (
 	EnvKeyProjectID          = "GCP_PROJECT"          // for Google Cloud Functions
-	EnvKeyGoogleCloudProject = "GOOGLE_CLOUD_PROJECT" // for Appengine
-	EnvKeyServiceName        = "GAE_SERVICE"
-	EnvKeyServiceVersion     = "GAE_VERSION"
+	EnvKeyGoogleCloudProject = "GOOGLE_CLOUD_PROJECT" // for Google App Engine
+	EnvKeyServiceName        = "GAE_SERVICE"          // for Google App Engine
+	EnvKeyServiceVersion     = "GAE_VERSION"          // for Google App Engine
 )
