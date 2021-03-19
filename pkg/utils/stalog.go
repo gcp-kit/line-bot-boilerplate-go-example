c9005fb57cf9cb7256720533507c77299c14e663
54m <30588003+54m@users.noreply.github.com>
2021-03-19 20:11:07 +0900

    feat: add stalog config

---
+++ pkg/utils/stalog.go
@@ -0,0 +1,27 @@
+package utils
+
+import (
+	"os"
+
+	"github.com/gcp-kit/line-bot-boilerplate-go-example/pkg/constant"
+	"github.com/gcp-kit/stalog"
+)
+
+// NewStalogConfig ...
+func NewStalogConfig(severities ...stalog.Severity) *stalog.Config {
+	severity := stalog.SeverityDefault
+	if len(severities) > 0 {
+		severity = severities[0]
+	}
+
+	config := stalog.NewConfig(GetProjectID())
+	config.RequestLogOut = os.Stderr               // request log to stderr
+	config.ContextLogOut = os.Stdout               // context log to stdout
+	config.Severity = severity                     // only over variable `severity` logs are logged
+	config.AdditionalData = stalog.AdditionalData{ // set additional fields for all logs
+		"service": GetServiceName(),
+		"version": GetServiceVersion(),
+		"build":   constant.CommitHash,
+	}
+	return config
+}
